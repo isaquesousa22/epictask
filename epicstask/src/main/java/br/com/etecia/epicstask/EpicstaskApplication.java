8d80a410733e181ac98c7d11d57a27d27eeb0644
isaquesousa22 <165092549+isaquesousa22@users.noreply.github.com>
2025-08-06 22:22:35 -0300

    Add files via upload

---
+++ epicstask/src/main/java/br/com/etecia/epicstask/EpicstaskApplication.java
@@ -0,0 +1,14 @@
+package br.com.etecia.epicstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicstaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicstaskApplication.class, args);
+	}
+
+}
+	
